e4f09edd8f69882b673b855a9d6edd2e06bbaa24
mingkang <49049296+kangming1412@users.noreply.github.com>
2019-06-30 21:51:46 -0700

    Update userPhotos.jsx

--- cs142/project5/components/userPhotos/userPhotos.jsx
+++ cs142/project5/components/userPhotos/userPhotos.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {
-  Typography
+  Typography, Card, ListItem, ListItemText, ListItemLink, CardMedia, CardContent
 } from '@material-ui/core';
 import './userPhotos.css';
+import { Link as RouterLink } from "react-router-dom";
+
 
 
 /**
@@ -13,8 +15,9 @@ class UserPhotos extends React.Component {
     super(props);
     console.log(window.cs142models.photoOfUserModel(this.props.match.params.userId));
 
-    this.state ={
-      usersPhoto: window.cs142models.photoOfUserModel(this.props.match.params.userId)
+    this.state = {
+      userPhotos: window.cs142models.photoOfUserModel(this.props.match.params.userId)
+      
     }
 
   }
@@ -22,17 +25,62 @@ class UserPhotos extends React.Component {
 
 
   render() {
+
     return (
-      <Typography variant="body1">
-      Since
-      it is invoked from React Router the params from the route will be
-      in property match. So this should show details of user:
-      {this.props.match.params.userId}. You can fetch the model for the user from
-      window.cs142models.photoOfUserModel(userId):
-        <Typography variant="caption">
-          {JSON.stringify(window.cs142models.photoOfUserModel(this.props.match.params.userId))}
-        </Typography>
-      </Typography>
+      <div  className = "cs142-userPhoto-container">
+        {this.state.userPhotos.map(photo => (
+          <div className="photos" key={photo._id}>
+            <Card>
+              <CardMedia
+                  component = "img"
+                  height="345"
+                  src={"/images/".concat(photo.file_name)}
+              />
+              <CardContent>
+
+                {/*show the date photo created*/}
+              <Typography gutterBottom variant="body1">
+                Created on {photo.date_time}
+              </Typography>
+              <br/>
+              <Typography variant="h5">
+                <strong>Comments</strong>
+              </Typography>
+              
+
+              {/*show the comments*/}
+              {photo.comments && (
+
+                 <Typography variant="body2">
+                {photo.comments.map(comment => {
+                  let link = "/users/" + comment.user._id;
+                  return (
+                    <div key={comment._id}>
+                        <ListItem>
+                          <ListItemText primary={comment.comment} 
+                                        secondary={comment.date_time}/>
+                        </ListItem>
+                        <ListItem 
+                            button component = {RouterLink} to = {link} >
+                            <ListItemText primary={comment.user.first_name}/> 
+                        </ListItem>
+                    </div>
+                        
+                  );
+                }
+
+                )}
+              </Typography>
+              )}
+             
+              </CardContent>
+            </Card>
+          </div>
+        ))}
+
+      </div>
+
+      
 
 
 
